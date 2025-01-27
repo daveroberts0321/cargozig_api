a5e7fb4a666b209f2859e3f2ab534012b9b8f152
daveroberts0321 <dave@tac-llc.com>
2025-01-27 06:35:40 -0600

    added rate limiting/finished itital setup

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/template/html/v2"
 	"github.com/joho/godotenv"
@@ -16,31 +18,35 @@ import (
 
 func main() {
 	fmt.Println("CargoZig Backend Golang")
-
 	// load .env file if it exists
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("Warning: .env file not found, using environment variables")
 	}
-
 	// Initialize database connection
 	db := config.GetDB()
-
 	// Initialize handlers with database
 	handlers.InitDB(db)
-
 	engine := html.New("./views", ".html")
 	// Pass the engine to the Fiber config
 	app := fiber.New(fiber.Config{
 		Views:       engine,
 		ViewsLayout: "layouts/main", // This will be your default layout
 	})
-
 	// Middleware
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{}))
+	// Initialize default config
+	app.Use(limiter.New(limiter.Config{
+		Max:               20,
+		Expiration:        30 * time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	})) // Limit the number of requests to 20 per 30 seconds
 
 	// Routes
-	app.Get("/", handlers.LandingPage)
+	// Group
+	admin := app.Group("/admin")
+
+	admin.Get("/", handlers.LandingPage)
 
 	// Start server
 	port := os.Getenv("PORT")
